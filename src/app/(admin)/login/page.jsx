b3c797f7237f48f895f5227c7538217e0131c198
codeHasib <mohammadhasib9827@gmail.com>
2026-05-02 21:24:28 +0600

    removed the redirection

--- src/app/(admin)/login/page.jsx
+++ src/app/(admin)/login/page.jsx
@@ -1,16 +1,16 @@
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
+// import { auth } from "@/lib/auth";
+// import { redirect } from "next/navigation";
+// import { headers } from "next/headers";
 import LoginPage from "@/components/admin/Login";
 
 export default async function Page() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+  // const session = await auth.api.getSession({
+  //   headers: await headers(),
+  // });
 
-  if (!session?.user) {
-    redirect("/admin/dashboard");
-  }
+  // if (!session?.user) {
+  //   redirect("/admin/dashboard");
+  // }
   return (
     <>
       <LoginPage></LoginPage>
