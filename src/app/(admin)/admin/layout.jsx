2af5d1a18c2a07f1689f5b11d94095ca935ab6bb
codeHasib <mohammadhasib9827@gmail.com>
2026-05-02 21:08:27 +0600

    removed products page from customer side

--- src/app/(admin)/admin/layout.jsx
+++ src/app/(admin)/admin/layout.jsx
@@ -1,19 +1,6 @@
-import { authClient, useSession } from "@/lib/auth-client";
 import Navigation from "@/components/admin/Navigation";
-import { redirect } from "next/navigation";
 
 export default function AdminLayout({ children }) {
-  // function auth() {
-  //   const { data, isPending } = useSession();
-
-  //   if (isPending) {
-  //     return <div>Loading</div>;
-  //   }
-
-  //   if (!data) {
-  //     return redirect("/");
-  //   }
-  // }
 
   return (
     <>
