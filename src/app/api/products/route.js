3dc08352c5647d182527a91bc62484d7b1ab0384
codeHasib <mohammadhasib9827@gmail.com>
2026-05-03 15:00:39 +0600

    add debuging version for get method in products api

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -3,7 +3,6 @@ import { Product } from "@/models/Product";
 import { uploadToCloudinary } from "@/lib/uploadToCloudinary";
 import { NextResponse } from "next/server";
 
-
 export const dynamic = "force-dynamic";
 
 // CREATE PRODUCT (WITH CLOUDINARY)
@@ -54,21 +53,42 @@ export async function POST(req) {
 }
 
 // GET PRODUCTS
+// export async function GET() {
+//   try {
+//     await connectDB();
+
+//     const products = await Product.find()
+//       .populate("categoryId")
+//       .sort({ createdAt: -1 });
+
+//     return NextResponse.json({
+//       success: true,
+//       products,
+//     });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { success: false, error: "Failed to fetch products" },
+//       { status: 500 },
+//     );
+//   }
+// }
+
+// temporary get products
 export async function GET() {
   try {
     await connectDB();
 
-    const products = await Product.find()
-      .populate("categoryId")
-      .sort({ createdAt: -1 });
+    const products = await Product.find().sort({ createdAt: -1 });
 
     return NextResponse.json({
       success: true,
       products,
     });
   } catch (error) {
+    console.error("🔥 PRODUCT GET ERROR:", error);
+
     return NextResponse.json(
-      { success: false, error: "Failed to fetch products" },
+      { success: false, error: error.message }, // 👈 IMPORTANT
       { status: 500 },
     );
   }
